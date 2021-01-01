9d3f6c7c0e30371a0274a9087ffbcf55a7d11771
Tal Zaken <67941052+tal-z@users.noreply.github.com>
2021-01-01 16:52:24 -0500

    Update SentenceWriter.py
    Fix up some punctuation and capitalization.

--- SentenceWriter.py
+++ SentenceWriter.py
@@ -5,6 +5,8 @@ import numpy as np
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 import re
 
+
+"""Step 1: Build dictionary of tagged words"""
 tagged_words = brown.tagged_words()
 tagged_words_dict = {}
 for tup in tagged_words:
@@ -14,26 +16,31 @@ for tup in tagged_words:
         tagged_words_dict[tup[1]] = []
         tagged_words_dict[tup[1]].append(tup[0].lower())
 
+'''Uncomment the following two lines if we just want to use the brown corpus's vocabulary, and ignore word frequency.'''
 for k,v in tagged_words_dict.items():
     tagged_words_dict[k] = list(set(v))
 
+"""Step 2: Choose a work, identify the author, and choose a sentence."""
 work = gutenberg.fileids()[np.random.randint(len(gutenberg.fileids()))]
 author = re.findall('(\w+)-',work)[0].title()
 
 sentences = gutenberg.sents(work)
 
-sentence_value = np.random.randint(len(sentences))
-rndm_sentence = sentences[sentence_value]
+rndm_sentence = sentences[np.random.randint(len(sentences))]
+
 tagged_rndm_sentence = pos_tag(rndm_sentence)
 
 
 new_sentence = []
 for tup in tagged_rndm_sentence:
-    if tup[1] in tagged_words_dict:
-        rndm_num = np.random.randint(len(tagged_words_dict[tup[1]]))
-        rndm_word = tagged_words_dict[tup[1]][rndm_num]
-        if rndm_word.isalpha():
-            new_word = rndm_word
+    if tup[0].isalpha():
+        if tup[1] in tagged_words_dict:
+            rndm_num = np.random.randint(len(tagged_words_dict[tup[1]]))
+            rndm_word = tagged_words_dict[tup[1]][rndm_num]
+            if rndm_word.isalpha():
+                new_word = rndm_word
+            else:
+                new_word = tup[0]
         else:
             new_word = tup[0]
     else:
@@ -41,8 +48,12 @@ for tup in tagged_rndm_sentence:
     new_sentence.append(new_word)
 
 
-new_sentence = TreebankWordDetokenizer().detokenize(new_sentence)
+new_sentence = str(TreebankWordDetokenizer().detokenize(new_sentence))
+new_sentence = new_sentence[0].upper() + new_sentence[1:]
+if  new_sentence[-1].isalnum():
+    new_sentence = new_sentence+'.'
 
+print(f"{author}:", tagged_rndm_sentence)
 print(f"{author}:", new_sentence)
 
 
