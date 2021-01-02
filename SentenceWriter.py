15c7147ea5859005ced18e8df479845a9b04d82c
Tal Zaken <67941052+tal-z@users.noreply.github.com>
2021-01-01 23:47:33 -0500

    Update SentenceWriter.py

--- SentenceWriter.py
+++ SentenceWriter.py
@@ -6,22 +6,23 @@ from nltk.tokenize.treebank import TreebankWordDetokenizer
 import re
 
 
-"""Step 1: Build dictionary of tagged words.
-"""
+"""Step 1: Build dictionary of tagged words."""
+
+tagged_words = brown.tagged_words()
+tagged_words_dict = {}
+for tup in tagged_words:
+    if tup[1] in tagged_words_dict:
+        tagged_words_dict[tup[1]].append(tup[0].lower())
+    else:
+        tagged_words_dict[tup[1]] = []
+        tagged_words_dict[tup[1]].append(tup[0].lower())
+
+'''Uncomment the following two lines if we just want to use the brown corpus's vocabulary, and ignore word frequency.'''
+for k, v in tagged_words_dict.items():
+    tagged_words_dict[k] = list(set(v))
+
 
 def write_sentence():
-    tagged_words = brown.tagged_words()
-    tagged_words_dict = {}
-    for tup in tagged_words:
-        if tup[1] in tagged_words_dict:
-            tagged_words_dict[tup[1]].append(tup[0].lower())
-        else:
-            tagged_words_dict[tup[1]] = []
-            tagged_words_dict[tup[1]].append(tup[0].lower())
-
-    '''Uncomment the following two lines if we just want to use the brown corpus's vocabulary, and ignore word frequency.'''
-    for k,v in tagged_words_dict.items():
-        tagged_words_dict[k] = list(set(v))
 
     """Step 2: Choose a work, identify the author, and choose a sentence."""
     work = gutenberg.fileids()[np.random.randint(len(gutenberg.fileids()))]
@@ -53,4 +54,8 @@ def write_sentence():
     if len(new_sentence) <= 3:
         return write_sentence()
     else:
-        return (author, new_detokenized_sentence)
+        return (author, re.sub('[\)]', '', new_detokenized_sentence))
+
+
+
+
